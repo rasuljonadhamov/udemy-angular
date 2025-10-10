67adf15f986227804b80fee1f065b91aca792a6e
rasuljon <rasuljonadxamov1212@gmail.com>
2025-10-10 23:13:15 +0500

    Update my-learning.component.ts

--- src/app/features/my-learning/my-learning.component.ts
+++ src/app/features/my-learning/my-learning.component.ts
@@ -1,16 +1,7 @@
-import {
-  Component,
-  OnInit,
-  OnDestroy,
-  inject,
-  signal,
-  effect,
-  EffectRef,
-} from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { CourseService } from '../../core/services/course.service';
-import { AuthStore } from '../../store/auth.store';
 import { Course } from '../../core/models/course.model';
 import { CourseCardComponent } from '../../shared/components/course-card/course-card.component';
 
@@ -21,36 +12,14 @@ import { CourseCardComponent } from '../../shared/components/course-card/course-
   templateUrl: './my-learning.component.html',
   styleUrl: './my-learning.component.css',
 })
-export class MyLearningComponent implements OnInit, OnDestroy {
+export class MyLearningComponent implements OnInit {
   private courseService = inject(CourseService);
-  private authStore = inject(AuthStore);
-  private destroyEffect: EffectRef | null = null;
 
   isLoading = signal(true);
   courses = signal<Course[]>([]);
   error = signal('');
 
   ngOnInit() {
-    this.loadPurchasedCourses();
-
-    this.destroyEffect = effect(() => {
-      const user = this.authStore.user();
-      if (user) {
-        this.loadPurchasedCourses();
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.destroyEffect) {
-      this.destroyEffect.destroy();
-    }
-  }
-
-  private loadPurchasedCourses() {
-    this.isLoading.set(true);
-    this.error.set('');
-
     this.courseService.getPurchasedCourses().subscribe({
       next: (courses) => {
         this.courses.set(courses);
@@ -59,7 +28,7 @@ export class MyLearningComponent implements OnInit, OnDestroy {
       error: (err) => {
         this.error.set(err.message || 'Failed to load your courses');
         this.isLoading.set(false);
-      },
+      }
     });
   }
 }
